2476f14a88fa1bb7dbe83eeaa2e7d48cface3e9c
podsni <podsni0@gmail.com>
2025-10-21 19:52:06 +0000

    feat: update plugin configurations for auto-save and codedium

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -1,23 +1,28 @@
-return {
-	"pocco81/auto-save.nvim",
-	config = function()
-		require("auto-save").setup({
-			enabled = true,
-			execution_message = {
-				message = function()
-					return "AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S")
-				end,
-				cleaning_interval = 0,
-			},
-			trigger_events = { "InsertLeave", "TextChanged" },
-			conditions = {
-				exists = true,
-				filename_is_not = {},
-				filetype_is_not = {},
-				modifiable = true,
-			},
-			write_all_buffers = false,
-			debounce_delay = 135,
-		})
-	end,
-}
+local plugin_config = require("plugin-config")
+
+return plugin_config.plugin("autosave", {
+    "pocco81/auto-save.nvim",
+    lazy = false, -- Load immediately
+    config = function()
+        require("auto-save").setup({
+            enabled = true,
+            execution_message = {
+                message = function()
+                    return "AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S")
+                end,
+                cleaning_interval = 0,
+            },
+            trigger_events = { "InsertLeave", "TextChanged" },
+            conditions = {
+                exists = true,
+                filename_is_not = {},
+                filetype_is_not = {},
+                modifiable = true,
+            },
+            write_all_buffers = false,
+            debounce_delay = 135,
+        })
+        
+        print("Auto-save plugin loaded and configured")
+    end,
+})
--- lua/plugins/codedium.lua
+++ lua/plugins/codedium.lua
@@ -1,9 +1,40 @@
-local M = {
-  'Exafunction/codeium.vim'
-}
+local plugin_config = require("plugin-config")
 
-function M.config()
-
-end
-
-return M
+return plugin_config.plugin("codedium", {
+    "Exafunction/codeium.vim",
+    lazy = false, -- Load immediately
+    config = function()
+        -- Codeium configuration
+        vim.g.codeium_disable_bindings = 1 -- Disable default keymaps
+        
+        -- Custom keymaps for Codeium
+        vim.keymap.set('i', '<C-g>', function()
+            return vim.fn['codeium#Accept']()
+        end, { expr = true, silent = true, desc = "Accept Codeium suggestion" })
+        
+        vim.keymap.set('i', '<C-;>', function()
+            return vim.fn['codeium#CycleCompletions'](1)
+        end, { expr = true, silent = true, desc = "Next Codeium suggestion" })
+        
+        vim.keymap.set('i', '<C-,>', function()
+            return vim.fn['codeium#CycleCompletions'](-1)
+        end, { expr = true, silent = true, desc = "Previous Codeium suggestion" })
+        
+        vim.keymap.set('i', '<C-x>', function()
+            return vim.fn['codeium#Clear']()
+        end, { expr = true, silent = true, desc = "Clear Codeium suggestion" })
+        
+        -- Status line integration
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "CodeiumStatusChanged",
+            callback = function()
+                vim.cmd("redrawstatus")
+            end,
+        })
+        
+        -- Enable Codeium
+        vim.cmd("CodeiumEnable")
+        
+        print("Codeium plugin loaded and configured")
+    end,
+})
